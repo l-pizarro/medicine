7263f83908eabd92b1d23680248be5fc8e8afc04
l-pizarro <leandro.pizarro@usach.cl>
2019-11-21 17:58:06 -0300

    Font do not work

--- src/views/Medicine.vue
+++ src/views/Medicine.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="medicine flex h-screen">
-    <aside class="px-6 bg-own-100">
+    <!-- <aside class="px-6 bg-own-100">
       <div class="mt-12 relative">
         <svg class="absolute h-12 ml-3" fill="#718097" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
         <input class="h-12 w-80 pl-12 pr-5 border border-gray-400 rounded-lg text-gray-600 font-light placeholder-gray-600 bg-gray-300 shadow-inner focus:outline-none" type="text">
@@ -63,7 +63,7 @@
         </div>
 
       </div>
-    </div>
+    </div> -->
   </div>
 </template> 
 
